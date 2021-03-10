f7b0c5101f5c963e4499c78590b17142b129aa39
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-10 07:54:07 +0400

    modifed to use pg-format for bulk insertions. currently 100,000 records at ~50 seconds. estimated time to 10,000,000 under 1 hour

--- database/postgres/seed.js
+++ database/postgres/seed.js
@@ -2,6 +2,7 @@ const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '../', '../', '.env')});
 //pg setup
 const { Client } = require('pg');
+const format = require('pg-format');
 const client = new Client();
 // premade raw queries
 const { 
@@ -35,19 +36,56 @@ const createTables = () => new Promise(async (resolve, reject) => {
   .catch(err => console.log(err))
 
 // generate data and save to tables
-const seed = async (numOfRecords) => {
+const seed = async (numOfRecords = 1000, batchSize = 1000) => {
   try {
+    const timestamp = Date.now();
     await connect();
     await dropExistingTables();
     await createTables();
+    let itemsBuffer = [];
+    let descriptionBuffer = [];
+    let infoBuffer = [];
+    let configurationBuffer = [];
+    let productBuffer = [];
+    let counter = 0;
     for(let i = 1; i <= numOfRecords; i++) {
-      let { similarItemsQuery, descriptionQuery, infoQuery, configurationQuery, productQuery } = pgRecord(i);
-      await client.query(similarItemsQuery);
-      await client.query(descriptionQuery);
-      await client.query(infoQuery)
-      await client.query(configurationQuery);
-      await client.query(productQuery);
+      // get values from record converter
+      let { similarItemsValues, descriptionValues, productValues, infoValues, configurationValues } = pgRecord(i);
+
+      // push values up to buffers
+      itemsBuffer.push(similarItemsValues);
+      descriptionBuffer.push(descriptionValues);
+      infoBuffer.push(infoValues);
+      configurationBuffer.push(configurationValues);
+      productBuffer.push(productValues);
+
+      // when counter reaches batch size insert rows and clear buffers
+      counter++;
+      if (counter === batchSize || (i === numOfRecords && counter !== 0)) {
+        const items = format('INSERT INTO similarItems (id, similarItems) VALUES %L', itemsBuffer);
+        await client.query(items);
+        itemsBuffer = [];
+
+        const descriptions = format('INSERT INTO descriptions(id, itemDescription) VALUES %L', descriptionBuffer);
+        await client.query(descriptions);
+        descriptionBuffer = [];
+
+        const info = format('INSERT INTO info(id, itemColor, brand, isPrimeFreeOneDay, isFreeDelivery) VALUES %L', infoBuffer)
+        await client.query(info);
+        infoBuffer = [];
+
+        const configuration = format('INSERT INTO configurations(id, configuration) VALUES %L', configurationBuffer);
+        await client.query(configuration);
+        configurationBuffer = [];
+
+        const product = format('INSERT INTO products(id, itemName) VALUES %L', productBuffer)
+        await client.query(product);
+        productBuffer = [];
+
+        counter = 0;
+      }
     }
+    console.log((Date.now() - timestamp) / 1000)
     await disconnect();    
   } catch (error) {
     console.log(error)
@@ -55,4 +93,4 @@ const seed = async (numOfRecords) => {
   }
 }
 
-seed(100);
+seed(100000, 1000);
