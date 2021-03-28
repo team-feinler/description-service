d8883a1d37bc17cb6104498acbd3b5ab00941436
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-28 13:35:29 +0400

    implemented genUpdate

--- server/controllers/postgres.js
+++ server/controllers/postgres.js
@@ -1,7 +1,6 @@
 const { getProduct } = require('../../database/postgres/database.js');
 const { asyncHandler, ErrAPI } = require('./utils.js');
-
-
+const { randomDoc } = require('../../database/postgres/data.js');
 
 exports.getProduct = asyncHandler( async(req, res, next) => {
   console.log('at controller')
@@ -9,6 +8,24 @@ exports.getProduct = asyncHandler( async(req, res, next) => {
   console.log('product fetched');
   res.status(200).json(product);
 });
+
+// takes product.id and updates the corresponding tables
+exports.updateProduct = asyncHandler( async(req, res, next) => {
+  const { itemDescription, configuration, similarItems, id, ...info} = req.body;
+  console.log(itemDescription, configuration, similarItems, info);
+  res.json(req.body);
+});
+
+// create random data for update if req.body is empty(for testing)
+exports.genUpdate = async (req, res, next) => {
+  if (!Object.keys(req.body).length) {
+    const {id, ...update} = randomDoc(parseInt(req.params.productId));
+    req.body = update;
+    next();
+  } else {
+    next();
+  }
+}
 
 
 
