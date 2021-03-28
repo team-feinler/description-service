8974a689e0c1394ca6f9a5c0e1987ddd17334531
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-28 13:31:41 +0400

    built basic reusable update query, not tested

--- database/postgres/database.js
+++ database/postgres/database.js
@@ -2,9 +2,11 @@ const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '../', '../', '.env')});
 const { Client } = require('pg');
 const client = new Client();
+const md5 = require('md5');
 
 client.connect()
 
+// get single product
 const getProductQuery = (productId) =>  `
   select 
     configurations.configuration,
@@ -23,11 +25,36 @@ const getProductQuery = (productId) =>  `
   where products.id = '${productId}';
 `;
 
-const updateQuery = (productId, update) => {
-  console.log(update);
-  return true;
+// build out query templates for CUD
+const updateQuery = ({table, columns, values, id}) => `
+  update ${table}
+    set
+    ${columns.map((col, i) => {
+      return `${col}='${values[i]}'`;
+    })}
+  where id='${id}'
+`;
+
+// const insertQuery 
+
+const queriesTypes = {
+  update: updateQuery
 }
 
+const combineUpdates = (type, ...queries) => `
+  BEGIN
+    ${queries.map(query => queriesTypes[type](query)).join('\n')}
+  COMMIT;
+`;
+
+const q = combineUpdates(
+  'update',
+  {table:'info', 
+  columns: ['brand', 'itemColor'], 
+  values: ['a', 'b'], 
+  id: '22b8837fc929b21ef4551e1ffa72f485'}
+  )//, {table:'test2', columns: ['col', 'test'], values: ['a', 'c'], id: 10})
+console.log(q)
 
 exports.getProduct = async (productId) => {
   const q = getProductQuery(productId);
