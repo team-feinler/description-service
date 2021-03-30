c1d87b4dd3b9aec6fb2844a4feb44b3537c20372
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-03-30 17:27:01 +0400

    implemented insert, delete, getBatch endpoints and data generator middlewares

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-require('../database/database.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const { descriptionById, descriptionByBatch, newDescription, genData, deleteDescription } = require('./controllers/mongo.js');
 
-const { getProduct, updateProduct, genUpdate, insertProduct, genInsert } = require('./controllers/postgres.js');
+const { getProduct, updateProduct, genUpdate, insertProduct, genInsert, deleteProduct, getProductBatch, genBatch, hashProductIds } = require('./controllers/postgres.js');
 const { errorHandler, hashParam } = require('./controllers/utils.js');
 
 const app = express();
@@ -35,14 +33,15 @@ app.use('/description/:productId', hashParam);
 app.route('/description/:productId')
   .get(getProduct)
   .put(genUpdate, updateProduct)
-  .delete(deleteDescription);
+  .delete(deleteProduct);
 
 // C controller
 app.route('/descriptions/new')
   .post(genInsert, insertProduct);
 
 //get multiple item descriptions
-app.get('/descriptions/multiple', descriptionByBatch);
+app.use(hashProductIds);
+app.get('/descriptions/multiple', genBatch, getProductBatch);
 app.use(errorHandler);
 
 module.exports = app;
